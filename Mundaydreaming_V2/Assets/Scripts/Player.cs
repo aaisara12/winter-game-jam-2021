eeb7d47c950fad857784a3e8ca4a81edea04511d
HiccupHan <89772977+HiccupHan@users.noreply.github.com>
2021-12-17 15:48:14 -0500

    Merge branch health into main

--- Mundaydreaming_V2/Assets/Scripts/Player.cs
+++ Mundaydreaming_V2/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public deathScreenScript deathScreen;
     public int playerMaxHealth = 100;
     public int playerCurrentHealth;
     public HealthBar healthBar;
@@ -31,6 +31,10 @@ public class Player : MonoBehaviour
     {
         playerCurrentHealth -= damage;
         healthBar.setHealth(playerCurrentHealth);
+        if (playerCurrentHealth == 0)
+        {
+            deathScreen.setDead();
+        }
     }
 
 }
