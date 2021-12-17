43c9e933d4ab0b175dc1d72da9542e4fe749ffad
wathmew <77905462+wathmew@users.noreply.github.com>
2021-12-17 13:26:25 -0800

    butter changes its angle as it flies

--- Mundaydreaming_V2/Assets/Scripts/projectile.cs
+++ Mundaydreaming_V2/Assets/Scripts/projectile.cs
@@ -8,25 +8,34 @@ public class projectile : MonoBehaviour
     [SerializeField] float lifetimeSeconds = 5;
     float timeStart = 0;
 
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
         timeStart = Time.time;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        trackMovement();
         if (Time.time - timeStart >= lifetimeSeconds)
             Destroy(this.gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
-        hitInfo.GetComponent<Player>()?.takeDamage(damage);
-        hitInfo.GetComponent<ToastScript>()?.takeDamage(damage);
-
+        hitInfo.GetComponent<PlayerStandIn>()?.takeDamage(damage);
         Destroy(this.gameObject);
+    }
+
+    void trackMovement()
+    {
+        Vector2 direction = rb.velocity;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
